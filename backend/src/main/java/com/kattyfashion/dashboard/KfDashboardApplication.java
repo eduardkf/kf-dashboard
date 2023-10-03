0ce091ea65362644e28903733488451a103bb0a0
Eduard Modreanu <eduard.modreanu@katty-fashion.ro>
2023-10-03 10:53:14 +0300

    first commit

---
+++ backend/src/main/java/com/kattyfashion/dashboard/KfDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.kattyfashion.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kattyfashion/dashboard/KfDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kattyfashion.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
